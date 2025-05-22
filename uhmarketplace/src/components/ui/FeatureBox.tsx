c87068dbd7886ec0c2413017e976a9d427acc0ce
Israel Trejo <isrtrejo07@gmail.com>
2025-05-21 21:21:16 -0500

    Fixed the dimensions of the feature boxes

--- uhmarketplace/src/components/ui/FeatureBox.tsx
+++ uhmarketplace/src/components/ui/FeatureBox.tsx
@@ -30,13 +30,13 @@ const FeatureBox: React.FC<FeatureBoxProps> = ({
     <div className="px-4">
       <div
         onClick={handleClick}
-        className="bg-white gap-2 rounded-xl p-2 transition-all duration-300 h-full flex flex-col items-center justify-center border-2 border-black-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] cursor-pointer hover:scale-105 transition-transform"
+        className="bg-white gap-2 rounded-xl p-2 transition-all duration-300 h-[200px] flex flex-col items-center justify-center border-2 border-black-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] hover:shadow-[0_8px_30px_rgb(0,0,0,0.12)] cursor-pointer hover:scale-105 transition-transform"
       >
         <div className="bg-gray-50 p-4 rounded-full mb-6">
           <img
             src={imageSrc}
             alt={altText}
-            className="w-20 h-auto object-contain"
+            className="w-20 h-auto object-contain h-[80px]"
           />
         </div>
         <p className="text-md md:text-xl font-geist-sans font-medium text-center mb-2">
